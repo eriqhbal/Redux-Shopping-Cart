844aca48bc550b003b120a5b96ed3221eed9a32c
Zuleriqhbal <eriqhbal@gmail.com>
2022-12-20 09:41:54 +0700

    Melakukan perubahan kode pada cartItems dan Layout

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
+// redux
+import { useSelector } from "react-redux";
+
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+    const cartItem = useSelector((state) => state.cart.itemsList);
+    console.log(cartItem);
   return (
     <div className="App">
-      <Auth />
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <Layout />}
+
       {/* <Layout /> */}
     </div>
   );
